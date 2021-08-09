482dbdaa062b39ef482cafdc1542c2eda6daac8e
Alex Boehm <alexboehm@systemli.org>
2021-08-09 20:13:27 +0930

    jijh

--- ex04/ft_strs_to_tab.c
+++ ex04/ft_strs_to_tab.c
@@ -1,9 +1,62 @@
-struct s_stock_str *ft_strs_to_tab(int ac, char **av)
-{
+#include "ft_stock_str.h"
+#include <stdlib.h>
 
+char	*ft_strdup(char *src)
+{
+	char	*copy;
+	int		i;
+
+	copy = malloc(sizeof(*src));
+	if (copy == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		copy[i] = src[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
 }
 
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+t_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int				i;
+	t_stock_str		*tab;
+
+	if (!(tab == (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1))))
+		return (0);
+	i = 0;
+	while (i < ac)
+	{
+		tab[i].size = ft_strlen(av[i]);
+		tab[i].str = av[i];
+		tab[i].copy = ft_strdup(av[i]);
+		i++;
+	}
+	tab[i].str = 0;
+	return (tab);
+}
+/*
+#include <stdio.h>
 int main(int ac, char **av)
 {
+	t_stock_str string;
+	string = *ft_strs_to_tab(ac, av);
+		
+	printf("%s", string.str);
 
+	printf("%s", string.copy);
+	printf("%d", string.size);
 }
+*/
