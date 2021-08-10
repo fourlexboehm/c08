77b50174489b91860f0e545819a3be7a28ee1fdd
Alex Boehm <alexboehm@systemli.org>
2021-08-10 15:20:12 +0930

    jnhk

--- ex04/ft_strs_to_tab.c
+++ ex04/ft_strs_to_tab.c
@@ -1,51 +1,59 @@
 #include "ft_stock_str.h"
 #include <stdlib.h>
-
 char	*ft_strdup(char *src)
 {
-	char	*copy;
+	char	*dest;
+	int		src_len;
 	int		i;
 
-	copy = malloc(sizeof(*src));
-	if (copy == NULL)
+	src_len = 0;
+	while (src[src_len])
+		src_len++;
+	dest = (char *)malloc(sizeof(char) * src_len + 1);
+	if (!dest)
 		return (NULL);
 	i = 0;
-	while (src[i] != '\0')
+	while (src[i])
 	{
-		copy[i] = src[i];
+		dest[i] = src[i];
 		i++;
 	}
-	copy[i] = '\0';
-	return (copy);
+	dest[i] = '\0';
+	return (dest);
 }
 
 int	ft_strlen(char *str)
 {
-	int	i;
+	int		i;
 
 	i = 0;
 	while (str[i])
+	{
 		i++;
+	}
 	return (i);
 }
 
-t_stock_str	*ft_strs_to_tab(int ac, char **av)
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
 {
+	t_stock_str		*s_av;
 	int				i;
-	t_stock_str		*tab;
 
-	if (!(tab == (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1))))
+	s_av = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!s_av)
 		return (0);
 	i = 0;
-	while (i < ac)
+	while (av[i] != 0 && i < ac)
 	{
-		tab[i].size = ft_strlen(av[i]);
-		tab[i].str = av[i];
-		tab[i].copy = ft_strdup(av[i]);
+		s_av[i].size = ft_strlen(av[i]);
+		s_av[i].str = av[i];
+		s_av[i].copy = ft_strdup(av[i]);
 		i++;
 	}
-	tab[i].str = 0;
-	return (tab);
+	s_av[i].size = 0;
+	s_av[i].str = 0;
+	s_av[i].copy = 0;
+	return (s_av);
 }
 /*
 #include <stdio.h>
@@ -53,9 +61,7 @@ int main(int ac, char **av)
 {
 	t_stock_str string;
 	string = *ft_strs_to_tab(ac, av);
-		
 	printf("%s", string.str);
-
 	printf("%s", string.copy);
 	printf("%d", string.size);
 }
