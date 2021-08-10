dc6744a6bdfbf9b78da220927ed1205aac7f7022
Alex Boehm <alexboehm@systemli.org>
2021-08-10 19:58:57 +0930

    hjk

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -1,13 +1,14 @@
 #ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
-# define TRUE 1
+# define TRUE  1
 # define FALSE 0
 # define SUCCESS 0
-# define EVEN(n) ((n%2 == 0)
-# define EVEN_MSG "I have an even number of arguments."
-# define ODD_MSG "I have an odd number of arguments."
+# define EVEN_MSG    "I have an even number of arguments.\n"
+# define ODD_MSG     "I have an odd number of arguments.\n"
+# define EVEN(nbr) (nbr % 2 == 0)
 
 void	ft_putstr(char *str);
+
 typedef int	t_bool;
 
 #endif
